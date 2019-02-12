77300715a9e65cf5cbf4cbbf984aa2e0ff045d56
Miguel Freire <m.b.freire@hotmail.com>
2019-02-12 11:22:29 +0000

    Cleaning

--- hs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --headless &
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9223 --headless &
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9224 --headless &
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9225 --headless &
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9226 --headless &
